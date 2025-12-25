89b932e270b6596bcb2cb1f244c50509268fc8cc
Фёдорова Василиса8 <vasilisa.f300@gmail.com>
2025-12-25 20:37:06 +0300

    Добавление модульного теста

---
+++ app/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿namespace app;
+
+public class UnitTest1
+{
+    [Fact]
+    public void PositiveTest()
+    {
+        //Arrange
+        double T0 = 100;
+        double Tenv = 20;
+        double k = 0.1;
+        double t = 0;
+        double ExpResult = 100;
+
+        //Act
+        double ActResult = NewtonCoolingModel.CalculateTemperature(T0, Tenv, k, t);
+
+        //Assert
+        Assert.Equal(ExpResult, ActResult);
+    }
+}
